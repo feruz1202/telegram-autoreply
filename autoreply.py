2919e2045a0823d02e73cf36a7553f74f2a5533e
Ikromov Feruz <ikromovferuz1202@gmail.com>
2026-05-17 12:46:43 +0500

    add debug logs

--- autoreply.py
+++ autoreply.py
@@ -3,7 +3,7 @@ from telethon import TelegramClient, events
 api_id = 36022953
 api_hash = 'd0da1904b3e98d0c784e7f7730c2cf23'
 
-DEFAULT_REPLY = "Salom! Hozir band man, tez orada javob beraman."
+DEFAULT_REPLY = "Salom! Bu Avto Javob. Hozir band man, tez orada javob beraman."
 
 client = TelegramClient('session', api_id, api_hash)
 
