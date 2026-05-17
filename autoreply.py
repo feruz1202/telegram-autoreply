26826de100f2fe2b6945c7f8e7e3259c95137703
Ikromov Feruz <ikromovferuz1202@gmail.com>
2026-05-17 12:42:51 +0500

    ignore bots

--- autoreply.py
+++ autoreply.py
@@ -3,12 +3,18 @@ from telethon import TelegramClient, events
 api_id = 36022953
 api_hash = 'd0da1904b3e98d0c784e7f7730c2cf23'
 
-DEFAULT_REPLY = "Salom! Bu Avto Javob. Hozir band man, tez orada javob beraman."
+DEFAULT_REPLY = "Salom! Hozir band man, tez orada javob beraman."
 
 client = TelegramClient('session', api_id, api_hash)
 
 @client.on(events.NewMessage(incoming=True, func=lambda e: e.is_private))
 async def handler(event):
+    sender = await event.get_sender()
+    
+    # Ignore bots
+    if sender.bot:
+        return
+    
     await event.reply(DEFAULT_REPLY)
 
 print("Auto-reply bot is running...")
